6b71a3c10a184727f9ecfb69c56f12c6d0c3fc98
eosqq <you@example.com>
2022-04-29 09:39:04 +0500

    add new component: AppNews

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,37 @@
     <div class="card center">
       <h3>Actually news {{ date }} </h3>
     </div>
+    <app-news
+      v-for="item in news"
+      :key="item.id"
+      :title="item.title"
+      :id="item.id"
+      :was-read="item.wasRead"
+      :is-open="isOpen"
+    ></app-news>
   </div>
 </template>
 
 <script>
+
+import AppNewsVue from './components/AppNews.vue'
+
 export default {
   data () {
     return {
-      date: new Date().toLocaleDateString()
-
+      date: new Date().toLocaleDateString(),
+      news: [
+        { title: 'news 1', id: 1, wasRead: false },
+        { title: 'news 2', id: 2, wasRead: false },
+        { title: 'news 3', id: 3, wasRead: false },
+        { title: 'news 4', id: 4, wasRead: false }
+      ],
+      isOpen: false
     }
+  },
+
+  components: {
+    'app-news': AppNewsVue
   }
 }
 </script>
---
+++ src/components/AppNews.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="card">
+        <h3> {{ title }} </h3>
+        <button class="btn" @click="isNewOpen = !isNewOpen"> {{ isNewOpen ? 'Закрыть' : 'Открыть' }} </button>
+        <p v-if="isNewOpen">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, dignissimos?</p>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    id: {
+      type: Number,
+      required: true
+    },
+    wasRead: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    isOpen: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+
+  data () {
+    return {
+      isNewOpen: this.isOpen
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
